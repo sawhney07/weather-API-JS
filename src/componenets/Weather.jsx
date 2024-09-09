b9453f8682dc56497137aec4896d5e4b4387fca1
Armaan <armaan070903@gmail.com>
2024-09-08 21:44:55 -0700

    fixed

--- src/componenets/Weather.jsx
+++ src/componenets/Weather.jsx
@@ -17,7 +17,7 @@ const Weather = () => {
       return;
     }
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=97f2785e97ecdf4faad443b7a2c296bc`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={your-api-key}`;
       const response = await fetch(url);
 
       if (!response.ok) {
